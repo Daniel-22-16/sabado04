8bfba272700214296e38757801f218d4e70f81d6
Daniel-22-16 <1475937@senati.pe>
2023-03-04 09:46:46 -0500

    sabado04

---
+++ sabado.py
@@ -0,0 +1,10 @@
+lista = ["red", "blue", "green", "white"]
+
+primer_valor = lista[0]
+
+ultimo_valor = lista[-1]
+
+
+print("el primer valor de la lsita es:", primer_valor)
+
+print("el ultimo valor de la lista es:", ultimo_valor)
